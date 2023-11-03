e3f74e619d0fb5b17ed5dfa1f1ff30f5a2e0cc9f
Aditya Tripathi <adityatripathi3906@gmail.com>
2023-11-03 23:18:37 +0530

    Sign Sense Working

---
+++ SIgnsense.py
@@ -0,0 +1,209 @@
+#!pip install tensorflow opencv-python mediapipe sklearn matplotlib
+import cv2
+import numpy as np
+import os as os
+from matplotlib import pyplot as plt
+import time
+import mediapipe as mp
+mp_holistic = mp.solutions.holistic # Holistic model
+mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False                  # Image is no longer writeable
+    results = model.process(image)                 # Make prediction
+    image.flags.writeable = True                   # Image is now writeable 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    return image, results
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connections
+def draw_styled_landmarks(image, results):
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                             ) 
+    # Draw right hand connections  
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                             ) 
+cap = cv2.VideoCapture(0)
+# Set mediapipe model 
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+
+        # Read feed
+        ret, frame = cap.read()
+
+        # Make detections
+        image, results = mediapipe_detection(frame, holistic)
+        print(results)
+        
+        # Draw landmarks
+        draw_styled_landmarks(image, results)
+
+        # Show to screen
+        cv2.imshow('SignSense', image)
+
+        # Break gracefully
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+len(results.left_hand_landmarks.landmark)
+results
+draw_landmarks(frame, results)
+plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+len(results.left_hand_landmarks.landmark)
+lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+def extract_keypoints(results):
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([lh, rh])
+result_test = extract_keypoints(results)
+result_test
+#468*3+33*4+
+21*3+21*3
+np.save('0', result_test)
+np.load('0.npy')
+# Path for exported data, numpy arrays
+DATA_PATH = os.path.join('MP_Data') 
+
+# Actions that we try to detect
+actions = np.array(['yes', 'no', 'hello', 'Help', 'I Love You', 'Nice To Meet You', 'thanks'])
+
+# Thirty videos worth of data
+no_sequences = 30
+
+# Videos are going to be 30 frames in length
+sequence_length = 30
+for action in actions: 
+    for sequence in range(no_sequences):
+        try: 
+            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+        except:
+            pass
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+label_map = {label:num for num, label in enumerate(actions)}
+label_map
+sequences, labels = [], []
+for action in actions:
+    for sequence in range(no_sequences):
+        window = []
+        for frame_num in range(sequence_length):
+            res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+np.array(sequences).shape
+np.array(labels).shape
+X = np.array(sequences)
+X.shape
+y = to_categorical(labels).astype(int)
+y
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+y_test.shape
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.callbacks import TensorBoard
+log_dir = os.path.join('Logs')
+tb_callback = TensorBoard(log_dir=log_dir)
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,126)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
+res = [.7, 0.2, 0.1]
+actions[np.argmax(res)]
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.fit(X_train, y_train, epochs=250, callbacks=[tb_callback])
+model.summary()
+res = model.predict(X_test)
+actions[np.argmax(res[4])]
+actions[np.argmax(y_test[4])]
+model.save('action.h5')
+model.load_weights('action.h5')
+from sklearn.metrics import multilabel_confusion_matrix, accuracy_score
+yhat = model.predict(X_train)
+ytrue = np.argmax(y_train, axis=1).tolist()
+yhat = np.argmax(yhat, axis=1).tolist()
+multilabel_confusion_matrix(ytrue, yhat)
+accuracy_score(ytrue, yhat)
+colors = [(245,117,16), (245,117,16), (245,117,16), (245,117,16), (245,117,16), (245,117,16), (245,117,16)]
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
+        cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        
+    return output_frame
+#sequence.reverse()
+#len(sequence)
+#sequence.append('def')
+#sequence.reverse()
+#sequence[-30:]
+# 1. New detection variables
+sequence = []
+sentence = []
+threshold = 0.8
+
+cap = cv2.VideoCapture(0)
+# Set mediapipe model 
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+
+        # Read feed
+        ret, frame = cap.read()
+
+        # Make detections
+        image, results = mediapipe_detection(frame, holistic)
+        print(results)
+        
+        # Draw landmarks
+        draw_styled_landmarks(image, results)
+        
+        # 2. Prediction logic
+        keypoints = extract_keypoints(results)
+#         sequence.insert(0,keypoints)
+#         sequence = sequence[:30]
+        sequence.append(keypoints)
+        sequence = sequence[-30:]
+        
+        if len(sequence) == 30:
+            res = model.predict(np.expand_dims(sequence, axis=0))[0]
+            print(actions[np.argmax(res)])
+            
+            
+        #3. Viz logic
+            if res[np.argmax(res)] > threshold: 
+                if len(sentence) > 0: 
+                    if actions[np.argmax(res)] != sentence[-1]:
+                        sentence.append(actions[np.argmax(res)])
+                else:
+                    sentence.append(actions[np.argmax(res)])
+
+            if len(sentence) > 5: 
+                sentence = sentence[-5:]
+
+            # Viz probabilities
+            image = prob_viz(res, actions, image, colors)
+            
+        cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
+        cv2.putText(image, ' '.join(sentence), (3,30), 
+                       cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        
+        # Show to screen
+        cv2.imshow('SignSense', image)
+
+        # Break gracefully
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()            
---
+++ readme.md
@@ -0,0 +1,10 @@
+Prerequisites
+must have python and the following modules installed on your pc:-
+install tensorflow, opencv-python, mediapipe, sklearn, matplotlib
+
+
+Step 1 - Extract all the zips
+Step 2 - Run Signsense.py 
+
+Contact Discord : cg_1shot
+Telegram : @clutchgod1shot 
